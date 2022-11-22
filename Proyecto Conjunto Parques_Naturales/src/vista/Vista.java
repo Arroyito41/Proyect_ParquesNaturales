cffdcba86cbe53ffc445a214e7f27896d4831764
Alvaro <Alvaro@DESKTOP-L6L3K8C>
2022-11-22 18:52:18 +0100

    modifico la clase vista

--- Proyecto Conjunto Parques_Naturales/src/vista/Vista.java
+++ Proyecto Conjunto Parques_Naturales/src/vista/Vista.java
@@ -16,8 +16,11 @@ import java.awt.Color;
 
 public class Vista extends JFrame {
 
-	private JPanel contentPane;
-
+	public JPanel contentPane, panelPrincipal,  panelCLM, panelInfo;
+	public JLabel lblLogo, lblParqueAlbacete, lblParqueCuenca, lblParqueGuada, lblParqueCiu, lblParqueToledo, lblMapaCLM, lblFiltro; 
+	public JTextPane txtpnInformacion; 
+	public JComboBox comboBoxFiltro;
+	public JList list;
 	/**
 	 * Launch the application.
 	 */
@@ -47,76 +50,76 @@ public class Vista extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JPanel panelPrincipal = new JPanel();
+		panelPrincipal = new JPanel();
 		panelPrincipal.setBackground(new Color(255, 250, 205));
 		panelPrincipal.setBounds(0, 0, 677, 560);
 		contentPane.add(panelPrincipal);
 		panelPrincipal.setLayout(null);
 		
-		JLabel lblLogo = new JLabel("");
+		lblLogo = new JLabel("");
 		lblLogo.setIcon(new ImageIcon("img/logoClm.PNG"));
 		lblLogo.setBounds(20, 11, 69, 69);
 		panelPrincipal.add(lblLogo);
 		
-		JPanel panelCLM = new JPanel();
+		panelCLM = new JPanel();
 		panelCLM.setBackground(new Color(199, 21, 133));
 		panelCLM.setBounds(263, 11, 404, 282);
 		panelPrincipal.add(panelCLM);
 		panelCLM.setLayout(null);
 		
-		JLabel lblParqueAlbacete = new JLabel("");
+		lblParqueAlbacete = new JLabel("");
 		lblParqueAlbacete.setIcon(new ImageIcon("img/chincheta.png"));
 		lblParqueAlbacete.setBounds(253, 183, 77, 45);
 		panelCLM.add(lblParqueAlbacete);
 		
-		JLabel lblParqueCuenca = new JLabel("");
+		lblParqueCuenca = new JLabel("");
 		lblParqueCuenca.setIcon(new ImageIcon("img/chincheta.png"));
 		lblParqueCuenca.setBounds(234, 106, 77, 45);
 		panelCLM.add(lblParqueCuenca);
 		
-		JLabel lblParqueGuada = new JLabel("");
+		lblParqueGuada = new JLabel("");
 		lblParqueGuada.setIcon(new ImageIcon("img/chincheta.png"));
 		lblParqueGuada.setBounds(172, 29, 77, 45);
 		panelCLM.add(lblParqueGuada);
 		
-		JLabel lblParqueCiu = new JLabel("");
+		lblParqueCiu = new JLabel("");
 		lblParqueCiu.setIcon(new ImageIcon("img/chincheta.png"));
 		lblParqueCiu.setBounds(118, 183, 77, 45);
 		panelCLM.add(lblParqueCiu);
 		
-		JLabel lblParqueToledo = new JLabel("");
+		lblParqueToledo = new JLabel("");
 		lblParqueToledo.setIcon(new ImageIcon("img/chincheta.png"));
 		lblParqueToledo.setBounds(95, 120, 77, 45);
 		panelCLM.add(lblParqueToledo);
 		
-		JLabel lblMapaCLM = new JLabel("");
+		lblMapaCLM = new JLabel("");
 		lblMapaCLM.setIcon(new ImageIcon("img/mapaclm.jpg"));
 		lblMapaCLM.setBounds(10, 11, 384, 260);
 		panelCLM.add(lblMapaCLM);
 		
-		JPanel panelInfo = new JPanel();
+		panelInfo = new JPanel();
 		panelInfo.setBounds(20, 96, 225, 197);
 		panelPrincipal.add(panelInfo);
 		panelInfo.setLayout(null);
 		
-		JTextPane txtpnInformacion = new JTextPane();
+		txtpnInformacion = new JTextPane();
 		txtpnInformacion.setEnabled(false);
 		txtpnInformacion.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		txtpnInformacion.setBounds(0, 0, 225, 197);
 		panelInfo.add(txtpnInformacion);
 		
-		JLabel lblFiltro = new JLabel("FILTRO:");
+		lblFiltro = new JLabel("FILTRO:");
 		lblFiltro.setForeground(new Color(0, 0, 139));
 		lblFiltro.setFont(new Font("Tahoma", Font.BOLD, 12));
 		lblFiltro.setBounds(20, 331, 54, 14);
 		panelPrincipal.add(lblFiltro);
 		
-		JComboBox comboBoxFiltro = new JComboBox();
+		comboBoxFiltro = new JComboBox();
 		comboBoxFiltro.setModel(new DefaultComboBoxModel(new String[] {"...", "CATEGOR\u00CDA", "PROVINCIA"}));
 		comboBoxFiltro.setBounds(96, 327, 168, 22);
 		panelPrincipal.add(comboBoxFiltro);
 		
-		JList list = new JList();
+		list = new JList();
 		list.setBounds(10, 370, 657, 179);
 		panelPrincipal.add(list);
 	}
