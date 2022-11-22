f88c77c657f265ab28110de2ec258b7a112a82c4
Alvaro <peco_alvaro@hotmail.com>
2022-11-22 14:12:25 +0100

    Relleno la clase parque y parques

--- Proyecto Conjunto Parques_Naturales/src/persistencias/Parque.java
+++ Proyecto Conjunto Parques_Naturales/src/persistencias/Parque.java
@@ -1,5 +1,54 @@
 package persistencias;
 
 public class Parque {
-
+	private long id_parque;
+	private String categoria;
+	private String provincia;
+	private String informacion;
+	
+	public Parque(long id_parque, String categoria, String provincia, String informacion) {
+		super();
+		this.id_parque = id_parque;
+		this.categoria = categoria;
+		this.provincia = provincia;
+		this.informacion = informacion;
+	}
+	
+	public Parque() {
+		super();
+	}
+	public long getId_parque() {
+		return id_parque;
+	}
+	public void setId_parque(long id_parque) {
+		this.id_parque = id_parque;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getProvincia() {
+		return provincia;
+	}
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+	public String getInformacion() {
+		return informacion;
+	}
+	public void setInformacion(String informacion) {
+		this.informacion = informacion;
+	}
+	
+	@Override
+	public String toString() {
+		return "Parque [id_parque=" + id_parque + ", categoria=" + categoria + ", provincia=" + provincia
+				+ ", informacion=" + informacion + "]";
+	}
+	
+	
+	
+	
 }
--- Proyecto Conjunto Parques_Naturales/src/persistencias/Parques.java
+++ Proyecto Conjunto Parques_Naturales/src/persistencias/Parques.java
@@ -1,5 +1,29 @@
 package persistencias;
 
-public class Parques {
+import java.util.ArrayList;
 
+public class Parques {
+	ArrayList <Parque> listaParques;
+
+	public Parques(ArrayList<Parque> listaParques) {
+		super();
+		this.listaParques = listaParques;
+	}
+
+	public Parques() {
+		super();
+	}
+
+	public ArrayList<Parque> getListaParques() {
+		return listaParques;
+	}
+
+	public void setListaParques(ArrayList<Parque> listaParques) {
+		this.listaParques = listaParques;
+	}
+
+	@Override
+	public String toString() {
+		return "Parques [listaParques=" + listaParques + "]";
+	}
 }
